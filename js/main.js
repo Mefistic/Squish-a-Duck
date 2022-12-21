45f750bc4507dd6a124698f55d6dd6bebffcf9ab
Mefistic <92448706+Mefistic@users.noreply.github.com>
2022-12-21 09:19:49 -0300

    SQUISH

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const duck_click_sound = new Audio(`sound/duckclick.wav`); duck_click_sound.volume = 0.33
+const duck_release_sound = new Audio(`sound/duckrelease.wav`); duck_release_sound.volume = 0.33
+
+document.getElementById(`duckhitbox`).onmousedown = () => {
+	console.log(`click`)
+	duck_click_sound.play()
+	document.getElementById(`duck`).style.transform = `scale(1, 0.75)`
+}
+document.getElementById(`duckhitbox`).onmouseup = () => {
+	console.log(`release`)
+	duck_release_sound.play()
+	document.getElementById(`duck`).style.transform = `scale(1)`
+}
+document.getElementById(`duckhitbox`).onmouseleave = document.getElementById(`duckhitbox`).onmouseup
